6666f1859db21de4570b66319b69671353fb613b
raphaelgenova <raphael.genova@gmail.com>
2020-05-07 18:33:07 -0300

    sharpe

--- indicators.py
+++ indicators.py
@@ -1,2 +1,6 @@
-def sharpe():
-    pass
+ def sharpe(df, rate_free):
+    #df é o dataframe com os dados
+    #rate_free é a taxa livre de risco
+    retorno = df['Close'].pct_change()
+    desvio = retorno.std()
+    return ((retorno[-1] - rate_free)/desvio)
